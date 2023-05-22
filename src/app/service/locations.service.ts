c3aa57a9085d5eb3db3c783d488e44443a05a089
datboiluiskrrt <datboiluiskrrt@gmail.com>
2023-05-21 19:39:01 -0500

    Able to get all states.

--- src/app/service/locations.service.ts
+++ src/app/service/locations.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { State } from '../models/locations.model';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocationsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  baseUrl = 'http://localhost:5266/api';
+  //get all cards
+  getAllLocations(): Observable<State[]> {
+    return this.http.get<State[]>(this.baseUrl + '/GetAllStates');
+  }
 }
