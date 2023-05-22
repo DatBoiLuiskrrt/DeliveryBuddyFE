a4e5abf7df54f985e584f22141aa98e47dc937f5
datboiluiskrrt <datboiluiskrrt@gmail.com>
2023-05-22 18:35:00 -0500

    Able to pull date from azure db.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { LocationsService } from './service/locations.service';
+import { State } from './models/locations.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DeliveryBuddyFE';
+  states: State[] = [];
+
+  constructor(private locationsService: LocationsService) {}
+  ngOnInit(): void {
+    this.getAllStateLocations();
+  }
+
+  getAllStateLocations() {
+    this.locationsService.getAllLocations().subscribe((response) => {
+      this.states = response;
+      console.log(response);
+    });
+  }
 }
---
+++ src/app/models/locations.model.ts
@@ -0,0 +1,6 @@
+export interface State {
+  id: string;
+  stateName: string;
+  stateCode: string;
+  countryId: number;
+}
