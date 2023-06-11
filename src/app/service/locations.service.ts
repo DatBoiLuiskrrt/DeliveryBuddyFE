700fe53f3fea6b31ec31e6ee53c0fbaf528ee115
Luis <datboiluiskrrt@gmail.com>
2023-06-11 11:41:47 -0500

    WIP

--- src/app/service/locations.service.ts
+++ src/app/service/locations.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { State } from '../models/locations.model';
@@ -8,9 +8,14 @@ import { State } from '../models/locations.model';
 export class LocationsService {
   constructor(private http: HttpClient) {}
 
-  baseUrl = 'http://localhost:5266/api';
-  //get all cards
+  baseUrl = 'https://localhost:7123/api';
+  //get all card
   getAllLocations(): Observable<State[]> {
     return this.http.get<State[]>(this.baseUrl + '/GetAllStates');
+  }
+
+  getAllCities(stateCode:any): Observable<any>{
+    let params = new HttpParams().set('Id', stateCode);
+    return this.http.get<any[]>(this.baseUrl + '/GetAllCitiesByStateId/', {params: params});
   }
 }
